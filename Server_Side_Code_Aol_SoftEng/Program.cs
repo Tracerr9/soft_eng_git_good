ae3487a8fad1b33ac9809cecd9455eb320a30f9a
Alvin Jonathan <jonathan.alvin2019@gmail.com>
2026-05-11 09:57:10 +0700

    menambah error handler global default asp.net web api

--- Server_Side_Code_Aol_SoftEng/Program.cs
+++ Server_Side_Code_Aol_SoftEng/Program.cs
@@ -57,6 +57,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
@@ -66,6 +67,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 app.UseHttpsRedirection();
 
